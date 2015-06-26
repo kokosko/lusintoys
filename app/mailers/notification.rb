dfb247eaccd546997feac68da50056dc7f026bf5
kokosko <kostya.kokosko@gmail.com>
2015-06-26 17:16:27 +0300

    mandrill domain

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,7 +1,10 @@
 class Notification < ActionMailer::Base
   default from: 'liuxintoys@gmail.com'
 
-  def order_notification
-    mail(to: 'liuxintoys@gmail.com', subject: t('notification.new'))
+  def order_notification(number)
+    @number = number
+    mail subject: t('notification.new') + ' ' + number.to_s,
+         to: 'liuxintoys@gmail.com',
+         from: 'liuxintoys@gmail.com'
   end
 end
